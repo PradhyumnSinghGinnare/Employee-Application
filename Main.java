cac4b3a12f3f0fffb347fb6c6d54a600483d1f81
PradhyumnSinghGinnare <128565492+PradhyumnSinghGinnare@users.noreply.github.com>
2024-02-01 22:35:00 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main{
+
+    EmployeeService service = new EmployeeService();
+    static boolean ordering = true;
+
+    public static void menu(){
+        System.out.println("\n******************Welcome To Employee Management System******************\n" + "\n1. Add Employee" + "\n2. View Employee" + "\n3. Update Employee" + "\n4. Delete Employee" + "\n5. View All Employee" + "\n6. Exit");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        EmployeeService service = new EmployeeService();
+
+        do{
+            menu();
+            System.out.println("Enter your choice");
+            int choice = sc.nextInt();
+
+            switch(choice){
+                case 1:
+                    System.out.println("Add Employee");
+                    service.addEmp();
+                    break;
+                case 2:
+                    System.out.println("View Employee");
+                    service.viewEmp();
+                    break;
+                case 3:
+                    System.out.println("Update Employee");
+                    service.updateEmp();
+                    break;
+                case 4:
+                    System.out.println("Delete Employee");
+                    service.deleteEmp();
+                    break;
+                case 5:
+                    System.out.println("View All Employee");
+                    service.viewAllEmp();
+                    break;
+                case 6:
+                    System.out.println("Thank You For Using The Application!!!");
+                    System.exit(0);
+                default:
+                    System.out.println("Please Enter Valid Choice!!");
+            }
+        }while(ordering);
+        sc.close();
+    }
+}
